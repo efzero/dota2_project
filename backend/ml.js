192e4cffa6dbec0550c203667a22c37b372792ec
Song <bsong18@fa17-cs411-22.cs.illinois.edu>
2017-12-09 16:06:21 -0600

    formatted files

---
+++ backend/ml.js
@@ -0,0 +1,88 @@
+
+var LinearRegression = require('shaman').LinearRegression;
+
+
+var ml = require('machine_learning')
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var mysql = require('mysql');
+var con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '11111111',
+	database :'dota2'
+
+});
+
+
+var str = ""; 
+
+var run_regression = function (input, x, y){
+
+			var lr = new LinearRegression(x, y, {
+				algorithm: 'GradientDescent',
+				numberOfIteration: 1000,
+				learning: 0.5
+				});
+
+			lr.train(function(err){
+				console.log(lr.costs);
+			});
+
+			var p = lr.predict(input);
+			return p;
+			
+			//svm.train({C: 1.0, tol: 1e-5, kernel: {type :'polynomial', c:1, d:5}});			//});
+
+}
+
+var knn = function(input){
+
+		return 0;
+	}
+
+
+var methods = {run_regression, knn}
+
+
+module.exports = methods;
+
+
+
+/*
+var data = [[1,0,1,0,1,1,1,0,0,0,0,0,1,0],
+            [1,1,1,1,1,1,1,0,0,0,0,0,1,0],
+            [1,1,1,0,1,1,1,0,1,0,0,0,1,0],
+            [1,0,1,1,1,1,1,1,0,0,0,0,1,0],
+            [1,1,1,1,1,1,1,0,0,0,0,0,1,1],
+            [0,0,1,0,0,1,0,0,1,0,1,1,1,0],
+            [0,0,0,0,0,0,1,1,1,0,1,1,1,0],
+            [0,0,0,0,0,1,1,1,0,1,0,1,1,0],
+            [0,0,1,0,1,0,1,1,1,1,0,1,1,1],
+            [0,0,0,0,0,0,1,1,1,1,1,1,1,1],
+            [1,0,1,0,0,1,1,1,1,1,0,0,1,0]
+           ];
+
+var result = [23,12,23,23,45,70,123,73,146,158,64];
+
+
+var knn = new ml.KNN({
+    data : data,
+    result : result
+});
+
+
+var y = knn.predict({
+    x : [0,0,0,0,0,0,0,1,1,1,1,1,1,1],
+    k : 10,
+ 
+    weightf : {type : 'gaussian', sigma : 10.0},distance : {type : 'euclidean'}});
+
+
+console.log(y);
+*/
+
+
---
+++ backend/server.js
@@ -0,0 +1,509 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var ml = require('./ml.js');
+var spawn = require('child_process').spawn;
+var fs = require('fs');
+
+
+
+
+
+var mysql = require('mysql');
+
+
+var str = ""; 
+var str2 = "";
+var str3 = "";
+
+var con;
+
+
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded());
+
+
+function handleDisconnect(){
+	con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '11111111',
+        database :'dota2'
+
+});
+
+	
+
+
+	con.connect(function(err){
+		if (err){
+			console.log('error connecting to db');
+			setTimeout(handleDisconnect, 2000);
+		}
+
+	});
+
+		con.on('error', function(err){
+
+			handleDisconnect();
+
+	});
+
+	
+	con.query("SELECT * FROM players", function(err, result, fields){
+			str = result;
+			});
+	con.query("SELECT DISTINCT (account_id), AVG(gold_per_min) as gold_per_min, AVG(xp_per_min) as xp_per_min, AVG(hero_damage) as hero_damage, AVG(stuns) as stuns FROM player GROUP BY account_id limit 100", function(err, result2, fields){
+			str2= result2;
+
+			});
+	con.query("SELECT match_id, duration, game_mode, radiant_win, positive_votes FROM matches limit 100", function(err, result3, fields){
+			str3 = result3;
+			});	
+}
+
+handleDisconnect();
+
+
+
+
+
+
+app.get('/getUser/:id', function(req, res){
+	console.log('starting api');
+	var target = mysql.escape(req.params.id);
+	console.log(target);
+	
+	var query = 'call proc(' + target + ')';
+	console.log(query);
+	con.query(query, function(err, result,fields){
+		if (err)
+			console.log(err);
+		res.send(result);
+
+	});
+	
+
+
+})
+
+
+
+
+app.get('/pageinate/:pages', function(req, res){
+	var target = req.params.pages;
+	
+	console.log(typeof(target));
+
+	if (isNaN(parseInt(target)) == false){
+		var lower_bound = (parseInt(target)-1)*100;
+		var upper_bound = lower_bound + 99;
+		
+		var query = 'SELECT DISTINCT (account_id), AVG(gold_per_min) as gpm, AVG(xp_per_min) as xpm, AVG(hero_damage) as hero_damage, AVG(stuns) as stuns FROM player WHERE account_id >= ' + lower_bound + ' AND account_id <= ' + upper_bound + '  GROUP BY account_id limit 100';
+
+		console.log(query);
+		
+		con.query(query, function(err, result, fields){
+			if (err)
+				console.log(err);
+			res.send(result);
+
+		})
+		
+	}
+	else
+		res.send('error');
+});
+
+
+
+
+
+
+app.get('/users', function(req, res){
+	console.log('iget');
+	console.log(str);
+	res.setHeader('Connection', 'keep-alive');
+	res.send(str);
+});
+
+app.get('/players', function(req, res){
+        console.log('iget');
+	console.log(str2);
+        res.setHeader('Connection', 'keep-alive');
+        res.send(str2);
+
+});
+
+
+app.get('/user/:userID', function(req, res){
+	console.log('nihaoma');
+	var target = mysql.escape(String(req.params.userID)) + " limit 50";
+	var result = "";
+
+	var query =  "SELECT account_id, gold_per_min as gold_per_min, xp_per_min as xp_per_min, hero_damage as hero_damage, stuns as stuns FROM player WHERE account_id = " + target;
+        var q = 'SELECT stuns FROM player WHERE account_id = 100';
+	con.query(query, function(err, result4, fields){
+            result= result4;
+	    res.send(result);
+
+        });
+
+})
+
+
+app.get('/matches/:matchID', function(req, res){
+	var target = String(req.params.matchID);
+	var result = "";
+	var query = "call search_matches ";
+	query = query + '(' + target + ')';
+	console.log(query);
+	con.query(query, function(err, result4, fields){
+		result = result4;
+		console.log(result);
+		res.send(result);
+
+	});
+
+});
+
+
+app.post('/insert-match', function(req, res){
+
+	var query = 'INSERT INTO matches(match_id, duration, game_mode, positive_votes, radiant_win) VALUES';
+	var infos = []
+	console.log(req.body);
+	for (i in req.body){
+		
+		if (i == 'wins' && (req.body[i] != 'True' && req.body.wins != 'False' )){
+			console.log(req.body[i]);
+			res.send('error in input');
+			return;
+		}
+
+		
+		else if (isNaN(parseFloat(req.body[i])) == true)
+			infos.push("'" + req.body[i] + "'");
+		else
+			infos.push(req.body[i]);
+
+	}
+
+	
+	var q = infos.join(',');
+	var target = " (" + q + ")";
+	query = query + target;
+	console.log(query)
+	
+	con.query(query, function(err, result4, fields){
+		var result;
+		if (err){
+		//	console.log(err);
+			result = err;
+		}
+		else{	//console.log(result4);
+			result = result4;
+		}
+		
+		res.send(result);
+	});
+
+
+
+});
+
+//run_regression([10,10,2,10]);
+
+
+
+app.get('/newuser', function(req, res){
+
+	var query = "SELECT account_id, gold_per_min, xp_per_min, kda FROM userPlayer limit 100";
+	var result = "";
+	console.log(query);
+	con.query(query, function(err, result4, fields){
+		result = result4;
+		res.send(result);
+
+	});
+
+})
+
+
+app.post('/insert', function(req, res){
+	res.setHeader('Connection','keep-alive');
+	console.log(req.body);
+	var query = "INSERT INTO userPlayer(account_id, gold_per_min, xp_per_min, kda) VALUES";
+	var query2 = "INSERT INTO player(account_id, gold_per_min, xp_per_min, hero_damage) VALUES";
+	var infos = []
+
+	var err = false;
+
+
+	for (i in req.body){
+		if (isNaN(parseFloat(req.body[i])) == true){
+			err= true
+			break;
+		}
+		else
+			infos.push(req.body[i]);
+	}	
+
+	if (!err){
+		var q = infos.join(',');
+		var target = "( " + q + " )"
+		query = query + target;
+		query2 = query2 + target;
+		console.log(query);
+		con.query(query, function(err, result4, fields){
+			con.query(query2, function(err5, result5, fields5){
+				console.log('want to know result5');
+				console.log(result5);
+				res.send("nimama");
+			});
+		//console.log(result4);
+		//res.send('nimama');	
+		});
+	}
+	else
+		res.send('error in input');
+
+});
+
+app.post('/update',function(req,res){
+	var query="UPDATE userPlayer SET gold_per_min= ";
+	var err=false;
+	var infos=[];
+	for (i in req.body){
+                if (isNaN(parseFloat(req.body[i])) == true){
+                        err= true
+                        break;
+                }
+                else
+                        infos.push(req.body[i]);
+        }
+	if(!err){
+		var q=infos;
+		var target=infos;
+		query=query+target;
+		console.log(query);
+		con.query(query,function(err,result4,fields){
+		res.send('nijiejie');
+		})
+	}
+	else
+		res.send('error in input');
+});
+
+
+app.post('/userUpdate', function(req, res){
+	var firstq = 'UPDATE player SET gold_per_min = ' + req.body['gold_per_min'] + ', account_id = ' +  req.body['account_id'] + ', hero_damage = ' + req.body['hero_damage']+ ', stuns = ' + req.body['stuns'] + ', xp_per_min = ' + req.body['xp_per_min'];
+	var secondq = ' WHERE gold_per_min = ' + mysql.escape(req.body['gpm']) + ' AND account_id = ' + mysql.escape(req.body['pid']) + ' AND xp_per_min =' + mysql.escape(req.body['xpm']) + ' AND stuns = ' + mysql.escape(req.body['stu']);
+
+	var err = false;
+
+//	console.log(firstq + secondq);
+	var query = firstq + secondq;
+	for (i in req.body){
+		if (isNaN(parseFloat(req.body[i])) == true){
+			err = true;
+			break
+		}
+
+	}	
+
+	if (!err || err){
+		console.log(query)
+		con.query(query, function(err, result4, fields){
+			console.log(result4);
+			console.log('suceess');
+			res.send('sucess');
+
+		})
+	}
+	else
+		res.send('error in input');
+
+
+});
+
+app.post('/delete', function(req,res){
+
+
+	var query="DELETE from userPlayer WHERE account_id=" ;
+	var err=false;
+	var infos=[];
+	console.log('deleting');
+	for(i in req.body){
+		if(isNaN(parseFloat(req.body[i]))==true){
+			err=true
+			break;
+		}
+		else continue;
+
+	}
+	if( !err){
+		var q=req.body['id'];
+		var target=mysql.escape(q);
+		query=query+target;
+		//console.log(q);
+		console.log(query);
+		con.query(query,function(err,result4,fields){
+		//console.log(result4);
+		console.log('deleted');
+		res.send('nibaba');
+		});
+	}
+	else 
+		res.send('error in input');
+});
+
+
+
+app.get('/matches', function(req, res){
+        res.setHeader('Connection', 'keep-alive');
+        res.send(str3);
+
+});
+
+var matrix;
+var y;
+
+app.post('/input-form', function(req, res){
+
+
+
+
+        
+	con.query("SELECT * FROM player limit 1000", function(err, result, fields){
+                        str = result
+                        console.log(str.length);
+                        var createMatrix = function(a){
+                                return [a.kills, a.assists, a.denies, a.deaths];
+
+                        }
+                        var createLabel = function(a){
+                                return [a.hero_damage];
+                        }
+
+
+                        matrix = result.slice(1).map(createMatrix);
+                        y = result.slice(1).map(createLabel);
+
+			var output = ml.run_regression(req.body, matrix, y);
+			console.log(output);
+			res.send(''+output);
+			});
+});
+
+
+
+
+
+app.post('/quick_query', function(req, res){
+	console.log(req.body);
+
+	console.log(matrix);
+	console.log(y);
+
+
+});
+
+
+
+app.post('/teammate', function(req,res){
+
+
+
+	 for (i in req.body){
+                if (isNaN(parseFloat(req.body[i])) == true){
+                        console.log('error');
+                        res.send('error');
+                        return;
+                }
+        }
+
+
+	if (!fs.existsSync('knn_model.sav')){
+
+
+	
+
+	con.query("SELECT * FROM player_ratings LIMIT 200000", function(err, result, fields){
+		
+		var py = spawn('python3',['-u', 'knn.py']);
+		
+		var create_Matrix = function(a){
+			return [a['account_id'], a['total_wins'], a['total_matches'], a['trueskill_mu'], a['trueskill_sigma']]
+		}
+		var id = [-1]
+		var query = id.concat(req.body);
+
+		
+		matrix = result.map(create_Matrix);
+		y = matrix.slice();
+		y.push(query)
+		py.stdin.write(JSON.stringify(y));
+		py.stdin.end();
+		var dataString = '';
+
+		py.stdout.on('data', function(data){
+        		dataString += data.toString();
+        		console.log("has red");
+        		console.log(dataString);
+  		});
+	
+		py.stdout.on('end', function(data){
+			console.log(dataString);
+			console.log('ending');
+			res.send(dataString);
+		});
+	});
+	}
+
+	else{
+		var py = spawn('python3',['-u', 'knn.py']);
+
+                var create_Matrix = function(a){
+                        return [a['account_id'], a['total_wins'], a['total_matches'], a['trueskill_mu'], a['trueskill_sigma']]
+                }
+                var id = [-1]
+                var query = id.concat(req.body);
+		
+                py.stdin.write(JSON.stringify(query));
+                py.stdin.end();
+                var dataString = '';
+
+                py.stdout.on('data', function(data){
+                        dataString += data.toString();
+                        console.log("has red");
+                        console.log(dataString);
+                });
+
+                py.stdout.on('end', function(data){
+                        console.log(dataString);
+                        console.log('ending');
+			con.query('SELECT * FROM player_ratings WHERE indices IN (' + dataString + ')', function(err, result, fields){
+				console.log(result);
+	
+				var final_result = result.map(create_Matrix);
+				console.log(final_result);
+                        	res.send(final_result);
+			});
+                });
+
+	}
+
+});
+
+
+
+
+
+
+
+
+app.listen(8080,  function(){
+	console.log('listening on 8080!')
+});
+
